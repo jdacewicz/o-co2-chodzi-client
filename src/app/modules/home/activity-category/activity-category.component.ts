49b0d2dbf8db80154e61199925b661ff55077854
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2024-09-28 17:45:31 +0200

    Created activity category

---
+++ src/app/modules/home/activity-category/activity-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-category',
+  standalone: true,
+  imports: [ActivityCategoryComponent],
+  templateUrl: './activity-category.component.html',
+  styleUrl: './activity-category.component.css'
+})
+export class ActivityCategoryComponent {
+
+}
