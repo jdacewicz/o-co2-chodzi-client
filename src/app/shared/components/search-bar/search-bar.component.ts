ff8f33994a000b04ac7672f72ecb54d406612e57
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2024-09-29 05:25:02 +0200

    Added footer

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Emission } from '../../interfaces/emission';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+  @Input() emissions: Emission[] = [];
+  @Output() emissionSelected = new EventEmitter<Emission[]>();
+  @Output() showCategory = new EventEmitter<void>();
+  searchTerm: string = '';
 
+  onSearch() {
+  }
+
+  handleKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.showCategory.emit(); // Emit event to show activity category
+    }
+  }
 }
