29b1d0f52c958ff65e40ca6ae045c53327e06b85
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2024-09-29 05:25:40 +0200

    Small changes in activities choose design

--- src/app/modules/home/activity-category/activity-category.component.ts
+++ src/app/modules/home/activity-category/activity-category.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-activity-category',
   standalone: true,
-  imports: [ActivityCategoryComponent],
+  imports: [ActivityCategoryComponent, CommonModule],
   templateUrl: './activity-category.component.html',
   styleUrl: './activity-category.component.css'
 })
 export class ActivityCategoryComponent {
+  isVisible = false;
 
+  toggleVisibility() {
+    this.isVisible = !this.isVisible;
+  }
 }
--- src/app/modules/home/activity-creation/activity-creation.component.ts
+++ src/app/modules/home/activity-creation/activity-creation.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { GoogleMapComponent } from '../../../shared/components/google-map/google-map.component';
 import { SearchBarComponent } from '../../../shared/components/search-bar/search-bar.component';
 import { ActivityCategoryComponent } from "../activity-category/activity-category.component";
+import { Emission } from '../../../shared/interfaces/emission';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-activity-creation',
   standalone: true,
-  imports: [GoogleMapComponent, SearchBarComponent, ActivityCategoryComponent],
+  imports: [GoogleMapComponent, SearchBarComponent, ActivityCategoryComponent, CommonModule],
   templateUrl: './activity-creation.component.html',
   styleUrl: './activity-creation.component.css'
 })
 export class ActivityCreationComponent {
+  emissions: Emission[] = [];
+  selectedEmission: Emission | null = null;
+  showCategory: boolean = false;
 
+  onEmissionSelected(emissions: Emission[]) {
+    if (emissions.length > 0) {
+      this.selectedEmission = emissions[0];
+    } else {
+      this.selectedEmission = null;
+    }
+  }
+
+  onShowCategory() {
+    this.showCategory = true;
+  }
 }
